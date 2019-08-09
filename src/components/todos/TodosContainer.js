1ba04a95212af1095c810ab01960e5e57a6b0434
Tyler Hunt <tyhunt04@gmail.com>
2019-08-09 18:43:37 +0000

    Automatically backed up by Learn

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import Todo from './todo'
 
 class TodosContainer extends Component {
 
